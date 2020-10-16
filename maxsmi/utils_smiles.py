3f0eab8f047d362a72f95d6cab7ccda9caa5f581
Talia Kimber <talia.kimber@gmail.com>
2020-10-16 14:54:19 +0200

    more on utility functions.

--- maxsmi/utils_smiles.py
+++ maxsmi/utils_smiles.py
@@ -8,12 +8,12 @@ Handles the primary functions
 def smi2can(smiles):
     """
     smi2can takes a SMILES and return its canonical form
-    
+
     Parameters
     ----------
     smiles: str
         SMILES string describing a compound.
-        
+
     Returns
     -------
         str: the canonical version of the SMILES
@@ -30,30 +30,30 @@ def smi2can(smiles):
         return None
     else:
         return Chem.MolToSmiles(mol, canonical=True, doRandom=False)
-    
-    
+
+
 def smi2rand(smiles, int_aug=50):
     """
     smi2rand takes a SMILES (non necessarily canonical)
     and returns n random variations of this SMILES.
-    
+
     Parameters
     ----------
     smiles: str
         SMILES string describing a compound.
     int_aug: int, Optional, default: 50
         the number of random SMILES generated.
-    
+
     Returns
     -------
         list: a list of int_aug random SMILES
             or None if the initial SMILES is not valid.
     """
-    
+
     from rdkit import Chem
     from rdkit.Chem import AllChem
 
-    mol  = Chem.MolFromSmiles(smile)
+    mol  = Chem.MolFromSmiles(smiles)
 
     if mol is None:
         # print('Faulty molecule in RDKit')
