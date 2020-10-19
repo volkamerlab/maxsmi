fd81ffae680589240e1778af166b339c928fe359
Talia Kimber <talia.kimber@gmail.com>
2020-10-19 14:04:23 +0200

    :sparkles: add uniqueness function.

--- maxsmi/utils_smiles.py
+++ maxsmi/utils_smiles.py
@@ -1,6 +1,6 @@
 """
 utils_smi.py
-Find the optimal SMILES augmentationn for accurate prediction.
+Find the optimal SMILES augmentation for accurate prediction.
 
 Handles the primary functions
 """
@@ -55,3 +55,40 @@ def smi2rand(smiles, int_aug=50):
         return None
     else:
         return [Chem.MolToSmiles(mol, canonical=False, doRandom=True) for nb in range(int_aug)]
+
+
+def smi2unique_rand(smiles, int_aug=50):
+    """
+    smi2uniquerand takes a SMILES (not necessarily canonical and
+    returns n unique random variations of this SMILES
+
+    Parameters
+    ----------
+    smiles: str
+        SMILES string describing a compound.
+    int_aug: int, Optional, default: 50
+        the number of random (may not be unique) SMILES generated.
+
+    Returns
+    -------
+        list: a list of unique random SMILES.
+    """
+
+    from rdkit import Chem
+    from rdkit.Chem import AllChem
+
+    mol  = Chem.MolFromSmiles(smiles)
+
+    if mol is None:
+        print('Faulty molecule in RDKit')
+        return None
+    else:
+        smi_unique = []
+        for nb in range(int_aug):
+            rand = Chem.MolToSmiles(mol, canonical=False, doRandom=True)
+            if rand in smi_unique:
+                pass
+            else:
+                smi_unique.append(rand)
+
+        return smi_unique
