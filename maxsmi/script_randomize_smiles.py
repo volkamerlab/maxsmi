dc39228fb6c776cb5f961670dabc185672c135b4
Talia Kimber <talia.kimber@gmail.com>
2020-10-19 14:05:21 +0200

    update script.

--- maxsmi/script_randomize_smiles.py
+++ maxsmi/script_randomize_smiles.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--nb_rand', type=int, help='nb_rand will be generated', default=3)
 args = parser.parse_args()
 
-from utils_smiles import smi2can, smi2rand
+from utils_smiles import smi2can, smi2rand, smi2unique_rand
 
 if __name__ == '__main__':
 
@@ -23,4 +23,9 @@ if __name__ == '__main__':
 
     ran_smi = smi2rand(smile, int_aug=args.nb_rand)
     print(f'List of {args.nb_rand} random SMILES')
-    print(ran_smi)
+    print(*ran_smi)
+
+    ran_unique_smi = smi2unique_rand(smile, int_aug=args.nb_rand)
+    print(*ran_unique_smi)
+    print(f'List of {len(ran_unique_smi)} out of '
+        f'{args.nb_rand} generated random SMILES')
