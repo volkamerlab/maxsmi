0ced949358315c9fb3916edd7aaa3409c9fd25d0
Talia Kimber <talia.kimber@gmail.com>
2021-01-05 16:50:28 +0100

    workflow running on pytorch.

--- maxsmi/full_workflow.py
+++ maxsmi/full_workflow.py
@@ -2,7 +2,8 @@
 From smiles to predictions
 
 """
-
+import numpy
+import os
 from utils_data import data_retrieval
 from utils_smiles import smi2can
 from utils_encoding import (
@@ -12,77 +13,168 @@ from utils_encoding import (
     one_hot_encode,
     pad_matrix,
 )
+from sklearn.model_selection import train_test_split
 import torch
-from pytorch_models import ConvolutionNetwork
 import torch.optim as optim
 import torch.nn as nn
+from torch.utils.data import TensorDataset
+from torch.utils.data.dataset import random_split
+from pytorch_models import ConvolutionNetwork
 
 
 if __name__ == "__main__":
 
-    # ================
-    # DATA
-    # ================
+    # ================================
+    # Data
+    # ================================
 
     # Read data
-    data = data_retrieval("ESOL")
-    ## TODO : check smiles and target columns for each data set
-    df = data[["measured log solubility in mols per litre", "smiles"]]
-    df = df.rename(columns={"measured log solubility in mols per litre": "ESOL"})
-
-    # ================
-    # Input processing
-    # ================
+    df = data_retrieval("ESOL")
 
     # Canonical SMILES
     df["canonical_smiles"] = df["smiles"].apply(smi2can)
 
-    # Replace double symbols
-    df["new_smiles"] = df["canonical_smiles"].apply(char_replacement)
+    # ================================
+    # Splitting
+    # ================================
 
-    # Obtain dictionary for this smile
-    smi_dict = get_unique_elements_as_dict(df["new_smiles"])
+    # Split data into train/test
+    test_ratio = 0.2
+    random_id = 1234
+
+    smiles_train, smiles_test, target_train, target_test = train_test_split(
+        df["canonical_smiles"],
+        df["target"],
+        test_size=test_ratio,
+        random_state=random_id,
+    )
+
+    # ================================
+    # Augmentation
+    # ================================
+
+    # TODO
+
+    smiles_aug_train = smiles_train
+    smiles_aug_test = smiles_test
+
+    # ================================
+    # Input processing
+    # ================================
+
+    # Replace double symbols
+    new_smi_train = smiles_aug_train.apply(char_replacement)
+    new_smi_test = smiles_aug_test.apply(char_replacement)
+
+    # Merge all smiles
+    all_smiles = new_smi_train.append(new_smi_test)
+
+    # Obtain dictionary for these smiles
+    smi_dict = get_unique_elements_as_dict(all_smiles)
     print("Number of unique characters: ", len(smi_dict))
 
-    # One-hot encode smiles
-    df["one_hot_smiles"] = df["new_smiles"].apply(one_hot_encode, dictionary=smi_dict)
-
-    # Obtain longest smiles
-    max_length_smi = get_max_length(df["new_smiles"])
+    # Obtain longest of all smiles
+    max_length_smi = get_max_length(all_smiles)
     print("Longest smiles in data set: ", max_length_smi)
 
+    # One-hot encode smiles on train and test
+    one_hot_train = new_smi_train.apply(one_hot_encode, dictionary=smi_dict)
+    one_hot_test = new_smi_test.apply(one_hot_encode, dictionary=smi_dict)
+
     # Pad for same shape
-    df["padded_ohe"] = df["one_hot_smiles"].apply(pad_matrix, max_pad=max_length_smi)
+    input_train = one_hot_train.apply(pad_matrix, max_pad=max_length_smi)
+    input_test = one_hot_test.apply(pad_matrix, max_pad=max_length_smi)
 
-    # ================
+    # ================================
     # Machine learning ML
-    # ================
+    # ================================
 
-    input_nn = torch.tensor(df["padded_ohe"])
-    print("Shape of input: ", input_nn.shape)
+    # ================================
+    # Pytorch data
+    # ================================
 
-    output_nn = torch.tensor(df["ESOL"])
-    output_nn = output_nn.view(-1, 1)
-    print("Shape of output: ", output_nn.shape)
+    # Train set
 
+    output_nn_train = torch.tensor(target_train.values).float()
+    output_nn_train = output_nn_train.view(-1, 1)
+    print("Shape of output: ", output_nn_train.shape)
+
+    input_nn_train = torch.tensor(list(input_train)).float()
+    print("Shape of input: ", input_nn_train.shape)
+
+    train_dataset = TensorDataset(input_nn_train, output_nn_train)
+
+    # Use mini batches
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=16, shuffle=True
+    )
+
+    # Initialize ml model
     ml_model = ConvolutionNetwork(nb_char=len(smi_dict), max_length=max_length_smi)
-    print(ml_model)
-    out = ml_model(input_nn.float())
-    print(out.shape)
+    print("Summary of ml model: ", ml_model)
 
     # Loss function
     loss_function = nn.MSELoss()
 
     # Use optimizer for objective function
-    optimizer = optim.SGD(ml_model.parameters(), lr=0.01)
+    optimizer = optim.SGD(ml_model.parameters(), lr=0.001)
 
-    # in your training loop:
-    optimizer.zero_grad()  # zero the gradient buffers
-    output = ml_model(input_nn.float())
-    loss = loss_function(out, output_nn)
-    # loss.backward()
-    # optimizer.step()  # Does the update
+    nb_epochs = 5
 
-    # Split data train - test
+    loss_per_epoch = []
+
     # Train model
-    # Evaluate on train & test
+    for epoch in range(nb_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader):
+
+            # True input/output
+            input_true, output_true = data
+
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+
+            # Forward
+            output_pred = ml_model(input_true)
+            # Objective
+            loss = loss_function(output_pred, output_true)
+            # Backward
+            loss.backward()
+            # Optimization
+            optimizer.step()
+            # Save loss
+            running_loss = +loss.item()
+
+        loss_per_epoch.append(running_loss / len(train_dataset))
+        print("Epoch : ", epoch + 1)
+        # print("Loss: ", loss_per_epoch)
+
+    # Save model
+    os.makedirs("saved_model", exist_ok=True)
+    torch.save(ml_model.state_dict(), "saved_model/model_dict.pth")
+
+    # ================================
+    # # Evaluate on test set
+    # ================================
+
+    # Load model
+    ml_model.load_state_dict(torch.load("saved_model/model_dict.pth"))
+
+    # Test set
+
+    output_nn_test = torch.tensor(target_test.values).float()
+    output_nn_test = output_nn_test.view(-1, 1)
+    input_nn_test = torch.tensor(list(input_test)).float()
+
+    test_dataset = TensorDataset(input_nn_test, output_nn_test)
+
+    # Test model
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=len(test_dataset)
+    )
+    with torch.no_grad():
+        for data in test_loader:
+            input_true_test, output_true_test = data
+            output_pred_test = ml_model(input_true_test)
+            loss_pred = loss_function(output_pred_test, output_true_test)
+            print("Loss on test set : ", loss_pred.item())
