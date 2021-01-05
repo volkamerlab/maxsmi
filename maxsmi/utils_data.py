7e52aefd9704bd57ed41662c13c67e32466fb2ad
Talia Kimber <talia.kimber@gmail.com>
2021-01-05 15:28:04 +0100

    add task for each dataset.

--- maxsmi/utils_data.py
+++ maxsmi/utils_data.py
@@ -21,22 +21,36 @@ def data_retrieval(target_data="ESOL"):
     Returns
     -------
     data: pd.Pandas
-        Pandas data frame.
+        Pandas data frame with two columns:
+            - `smiles`: SMILES encoding of the compounds.
+            - `target`: the measured target values.
     """
 
     if target_data == "ESOL":
         url = "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/delaney-processed.csv"
+        data = pd.read_csv(url)
+        task = "measured log solubility in mols per litre"
+
     elif target_data == "free solv":
         url = "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/SAMPL.csv"
+        data = pd.read_csv(url)
+        task = "expt"
+
     elif target_data == "lipophilicity":
         url = (
             "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/Lipophilicity.csv"
         )
+        data = pd.read_csv(url)
+        task = "exp"
+
     else:
         print("Invalid data. Choosing ESOL by default. ")
         url = "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/delaney-processed.csv"
+        data = pd.read_csv(url)
 
-    data = pd.read_csv(url)
-    print("Shape of data set: ", data.shape)
+    df = data[[task, "smiles"]]
+    df = df.rename(columns={task: "target"})
 
-    return data
+    print("Shape of data set: ", df.shape)
+
+    return df
