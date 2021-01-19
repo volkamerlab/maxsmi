f4ee5ace58b9bb9d55f73cf7750a7ea6714f6e39
Talia Kimber <talia.kimber@gmail.com>
2021-01-19 15:21:08 +0100

    :white_check_mark: add tests.

---
+++ maxsmi/tests/test_utils_data.py
@@ -0,0 +1,31 @@
+"""
+Unit and regression test for the maxsmi package.
+"""
+
+# Import package, test suite, and other packages as needed
+# import maxsmi
+import pytest
+import sys
+import pandas as pd
+
+from maxsmi.utils_data import data_retrieval, augmented_data
+
+
+def test_maxsmi_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "maxsmi" in sys.modules
+
+
+####################
+@pytest.mark.parametrize(
+    "task, solution",
+    [
+        ("ESOL", "OCC3OC(OCC2OC(OC(C#N)c1ccccc1)C(O)C(O)C2O)C(O)C(O)C3O "),
+        ("lipophilicity", "Cn1c(CN2CCN(CC2)c3ccc(Cl)cc3)nc4ccccc14"),
+        ("dncn", "OCC3OC(OCC2OC(OC(C#N)c1ccccc1)C(O)C(O)C2O)C(O)C(O)C3O "),
+        ("free solv", "CN(C)C(=O)c1ccc(cc1)OC"),
+    ],
+)
+def test_data_retrieval(task, solution):
+    dataframe = data_retrieval(task)
+    assert solution == dataframe.smiles[0]
---
+++ maxsmi/tests/test_utils_evaluation.py
@@ -0,0 +1,25 @@
+"""
+Unit and regression test for the maxsmi package.
+"""
+
+# Import package, test suite, and other packages as needed
+# import maxsmi
+import pytest
+import sys
+import torch
+from maxsmi.utils_evaluation import evaluation_results
+
+
+def test_maxsmi_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "maxsmi" in sys.modules
+
+
+####################
+@pytest.mark.parametrize(
+    "pred_output, true_output, solution",
+    [(torch.zeros(2), torch.zeros(2), (0, 0, 1))],
+)
+def test_smi2can(pred_output, true_output, solution):
+    results = evaluation_results(true_output, pred_output)
+    assert solution == results
