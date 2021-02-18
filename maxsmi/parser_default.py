670958e0e32b09338f93d01d2cde9282ffe9594b
Talia Kimber <talia.kimber@gmail.com>
2021-02-18 12:14:25 +0100

    files for constants and model parameters.

---
+++ maxsmi/parser_default.py
@@ -0,0 +1,26 @@
+"""
+parser_default.py
+The default parameters for parser.
+"""
+
+from augmentation_strategies import no_augmentation
+
+
+# Data and related task
+TASK = "ESOL"
+
+# Molecular string encoding
+STRING_ENCODING = "smiles"
+
+# Augmentation strategy for the train and test sets
+AUGMENTATION_STRATEGY = no_augmentation
+
+# Augmentation on train and test sets
+TRAIN_AUGMENTATION = 10
+TEST_AUGMENTATION = 10
+
+# The machine learning model
+ML_MODEL = "CONV1D"
+
+# If ensemble learning is applied in the evaluation
+ENSEMBLE_LEARNING = True
---
+++ maxsmi/splitting_parameters.py
@@ -0,0 +1,8 @@
+"""
+splitting_parameters.py
+Parameters for splitting into train and test
+"""
+
+
+TEST_RATIO = 0.2
+RANDOM_SEED = 1234
