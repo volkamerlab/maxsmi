2432b58947a0484c55cc01177d871b412d7b21b7
t-kimber <talia.kimber@gmail.com>
2021-05-28 11:25:45 +0200

    explicit naming for utils functions.

--- examples/augmentation_ESOL.py
+++ examples/augmentation_ESOL.py
@@ -7,7 +7,7 @@ Given the ESOL data set, generate augmentation.
 
 import argparse
 from maxsmi.utils_data import data_retrieval
-from maxsmi.utils_smiles import smi2can, smi2rand
+from maxsmi.utils_smiles import smiles_to_canonical, smiles_to_random
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--nb_rand", type=int, help="nb_rand will be generated", default=10)
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     df = data_retrieval("ESOL")
 
     # Canonical SMILES
-    df["canonical_smiles"] = df["smiles"].apply(smi2can)
+    df["canonical_smiles"] = df["smiles"].apply(smiles_to_canonical)
 
     # Random SMILES
-    df["random_smiles"] = df["canonical_smiles"].apply(smi2rand, args=(2,))
+    df["random_smiles"] = df["canonical_smiles"].apply(smiles_to_random, args=(2,))
 
     print(df.shape, df.columns)
     print(df.head(2))
