15f85bf38a1298741d5b0202f38739245a357cd4
t-kimber <talia.kimber@gmail.com>
2021-05-28 14:51:41 +0200

    add training loop.

--- maxsmi/pytorch_training.py
+++ maxsmi/pytorch_training.py
@@ -5,38 +5,67 @@ Pytorch training loop and necessary parameters.
 """
 import logging
 import torch
+import torch.optim as optim
 
 from maxsmi.pytorch_data import data_to_pytorch_format
 
 BACTH_SIZE = 16
 LEARNING_RATE = 0.001
-NB_EPOCHS = 250
+NB_EPOCHS = 2
 
 
 def model_training(
     data_loader,
+    ml_model_name,
     ml_model,
     loss_function,
-    optimizer,
     nb_epochs,
     is_cuda,
-    len_full_data,
-    smi_dict,
-    max_length_smi,
-    device,
+    len_train_data,
+    smiles_dictionary,
+    max_length_smiles,
+    device_to_use,
+    learning_rate,
 ):
     """
-    # TODO
+    Parameters
+    ----------
+    data_loader : torch.utils.data
+        The training data as seen by Pytorch for mini-batches.
+    ml_model_name : str
+        Name of the machine learning model. It can be either "CON1D", "CONV2D", or "RNN".
+    ml_model : nn.Module
+        Instance of the pytorch machine learning model.
+    loss_function : torch.nn.modules.loss
+        The loss function to be optimized.
+    nb_epochs : int
+        Number of epochs to train the model.
+    is_cuda : bool
+        Is cuda available.
+    len_train_data :
+        Number of data points in the training set.
+    smiles_dictionary : dict
+        The dictionary of SMILES characters.
+    max_length_smiles : int
+        The length of the longest SMILES.
+    device_to_use : torch.device
+        The device to use for model instance, "cpu" or "cuda".
+    learning_rate : float
+        The learning rate in the optimization algorithm.
+
+    Returns
+    -------
+    list
+        The loss value at each epoch.
     """
-    logging.info("========")
-    logging.info(f"Training for {nb_epochs} epochs")
-    logging.info("========")
+
+    optimizer = optim.SGD(ml_model.parameters(), lr=learning_rate)
 
     ml_model.train()
     loss_per_epoch = []
     for epoch in range(nb_epochs):
         running_loss = 0.0
-        for i, data in enumerate(data_loader):
+        for _, data in enumerate(data_loader):
 
             # SMILES and target
             smiles, target = data
@@ -44,10 +73,10 @@ def model_training(
             input_true, output_true = data_to_pytorch_format(
                 smiles,
                 target,
-                smi_dict,
-                max_length_smi,
-                ml_model,
-                device,
+                smiles_dictionary,
+                max_length_smiles,
+                ml_model_name,
+                device_to_use,
             )
 
             # Zero the parameter gradients
@@ -65,7 +94,7 @@ def model_training(
             # free memory
             del data
 
-        loss_per_epoch.append(running_loss / len_full_data)
+        loss_per_epoch.append(running_loss / len_train_data)
         if epoch % 10 == 0:
             logging.info(f"Epoch : {epoch + 1} ")
 
