35715e2f5a2e0cd89e4aff0e0e7acaa6e1a05788
t-kimber <talia.kimber@gmail.com>
2021-05-30 19:47:20 +0200

    ✅ add tests.

--- maxsmi/tests/test_utils.py
+++ maxsmi/tests/test_utils.py
@@ -6,7 +6,14 @@ Unit and regression test for the maxsmi package.
 # import maxsmi
 import pytest
 import sys
-from maxsmi.utils import string_to_bool
+from maxsmi.utils import string_to_bool, augmentation_strategy
+from maxsmi.augmentation_strategies import (
+    no_augmentation,
+    augmentation_with_duplication,
+    augmentation_without_duplication,
+    augmentation_with_reduced_duplication,
+    augmentation_maximum_estimation,
+)
 
 
 def test_maxsmi_imported():
@@ -22,3 +29,31 @@ def test_maxsmi_imported():
 def test_string_to_bool(string, solution):
     result = string_to_bool(string)
     assert solution == result
+
+
+def test_string_to_bool_exception():
+    with pytest.raises(Exception):
+        assert string_to_bool("Fasle")
+
+
+@pytest.mark.parametrize(
+    "string, solution",
+    [
+        ("no_augmentation", no_augmentation),
+        ("augmentation_with_duplication", augmentation_with_duplication),
+        ("augmentation_without_duplication", augmentation_without_duplication),
+        (
+            "augmentation_with_reduced_duplication",
+            augmentation_with_reduced_duplication,
+        ),
+        ("augmentation_maximum_estimation", augmentation_maximum_estimation),
+    ],
+)
+def test_augmentation_strategy(string, solution):
+    result = augmentation_strategy(string)
+    assert solution == result
+
+
+def test_augmentation_strategy_exception():
+    with pytest.raises(Exception):
+        assert augmentation_strategy("augment")
