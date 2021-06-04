403aa782171d210c52683656bb9e283f8a7335cd
t-kimber <talia.kimber@gmail.com>
2021-06-04 10:44:13 +0200

    :white_check_mark: Update tests.

--- maxsmi/tests/test_utils_evaluation.py
+++ maxsmi/tests/test_utils_evaluation.py
@@ -6,7 +6,7 @@ Unit and regression test for the maxsmi package.
 # import maxsmi
 import pytest
 import sys
-import torch
+import numpy
 from maxsmi.utils_evaluation import evaluation_results
 
 
@@ -17,12 +17,11 @@ def test_maxsmi_imported():
 
 ####################
 @pytest.mark.parametrize(
-    "pred_output, true_output, cuda_available, solution",
+    "pred_output, true_output, solution",
     [
-        (torch.zeros(2), torch.zeros(2), False, (0, 0, 1)),
-        (torch.zeros(2), torch.zeros(2), True, (0, 0, 1)),
+        (numpy.zeros(2), numpy.zeros(2), (0, 0, 1)),
     ],
 )
-def test_evaluation_results(pred_output, true_output, cuda_available, solution):
-    results = evaluation_results(true_output, pred_output, cuda_available)
+def test_evaluation_results(pred_output, true_output, solution):
+    results = evaluation_results(true_output, pred_output)
     assert solution == results
