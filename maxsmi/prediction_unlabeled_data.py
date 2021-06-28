7aab7ee1e7fe9a26c593d2076f795615c46ee5ad
t-kimber <talia.kimber@gmail.com>
2021-06-28 16:25:03 +0200

    update prediction script.

--- maxsmi/prediction_unlabeled_data.py
+++ maxsmi/prediction_unlabeled_data.py
@@ -14,10 +14,18 @@ import rdkit
 from rdkit.Chem import Draw
 import torch
 
-from maxsmi.utils_data import data_retrieval, smiles_in_training
-from maxsmi.utils_smiles import smiles_to_canonical, ALL_SMILES_CHARACTERS
+from maxsmi.utils_data import data_retrieval, smiles_in_training, data_checker
+from maxsmi.utils_smiles import (
+    smiles_to_canonical,
+    identify_disconnected_structures,
+    ALL_SMILES_CHARACTERS,
+)
 from maxsmi.utils_encoding import char_replacement
-from maxsmi.utils_prediction import retrieve_optimal_model
+from maxsmi.utils_prediction import (
+    retrieve_optimal_model,
+    unlabeled_smiles_max_length,
+    mixture_check,
+)
 
 from maxsmi.pytorch_models import model_type
 from maxsmi.pytorch_data import AugmentSmilesData
@@ -42,6 +50,12 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+
+    # SMILES checker
+    mixture_check(args.new_smiles)
+
+    # Data checker
+    data_checker(args.task)
 
     folder = f"maxsmi/prediction/{args.task}"
     os.makedirs(folder, exist_ok=True)
@@ -74,25 +88,31 @@ if __name__ == "__main__":
 
     # Read data
     data = data_retrieval(args.task)
+    logging.info(f"Shape of training data set before processing: {data.shape} ")
 
-    # Create data frame with SMILES
+    # Canonical SMILES
+    data["canonical_smiles"] = data["smiles"].apply(smiles_to_canonical)
+    data["disconnected_smi"] = data["canonical_smiles"].apply(
+        identify_disconnected_structures
+    )
+    data = data.dropna(axis=0)
+    data = data.drop(["disconnected_smi", "smiles"], axis=1)
+
+    logging.info(f"Shape of training data set after processing: {data.shape} ")
+
+    # Create data frame with unlabeled SMILES
     new_data = pandas.DataFrame(columns=["target", "smiles"])
     new_data.loc[0] = [numpy.nan, args.new_smiles]
 
     new_data["smiles_in_training"] = new_data["smiles"].apply(
         smiles_in_training, args=(data,)
     )
-    logging.info(f"SMILES in training set: {new_data.loc[0, 'smiles_in_training']} ")
+    logging.info(
+        f"SMILES in training data set: {new_data.loc[0, 'smiles_in_training']} "
+    )
 
     # Canonical SMILES
     new_data["canonical_smiles"] = new_data["smiles"].apply(smiles_to_canonical)
-    # new_data["disconnected_smi"] = new_data["canonical_smiles"].apply(
-    #     identify_disconnected_structures
-    # )
-    # data = data.dropna(axis=0, subset=["disconnected_smi"])
-    # data = data.drop(["disconnected_smi", "smiles"], axis=1)
-
-    logging.info(f"Shape of data set: {data.shape} ")
 
     # ================================
     # String encoding & Augmentation
@@ -106,13 +126,13 @@ if __name__ == "__main__":
         longest_smiles,
     ) = retrieve_optimal_model(args.task)
 
-    if len(args.new_smiles) > longest_smiles:
-        # logging.info(f"The SMILES is too long for this model. Progam aborting")
-        raise ValueError("The SMILES is too long for this model. Progam aborting")
-
     new_data["augmented_smiles"] = new_data["canonical_smiles"].apply(
         augmentation_strategy, args=(augmentation_number,)
     )
+
+    logging.info(f"Augmentation strategy: {augmentation_strategy.__name__}")
+    logging.info(f"Augmentation number: {augmentation_number}")
+
     time_end_augmenting = datetime.now()
     time_augmenting = time_end_augmenting - time_start_augmenting
     logging.info(f"Time for augmentation {time_augmenting}")
@@ -124,23 +144,20 @@ if __name__ == "__main__":
     # Replace double symbols
     new_data["new_smiles"] = new_data["augmented_smiles"].apply(char_replacement)
 
-    # Obtain dictionary for these smiles
+    # Retrieve SMILES' dictionary
     smi_dict = ALL_SMILES_CHARACTERS
-    logging.info(f"Number of unique characters: {len(smi_dict)} ")
-    logging.info(f"String dictionary: {smi_dict} ")
 
-    # TODO
     # Obtain longest of all smiles
     max_length_smi = longest_smiles
-    # Check if new smiles are longer that the longest smiles
-    logging.info(f"Longest smiles in data set: {max_length_smi} ")
+    new_data["new_smiles"].apply(unlabeled_smiles_max_length, args=(max_length_smi,))
+    logging.info(f"Longest smiles in training data set: {max_length_smi} ")
 
     # ==================================
     # Machine learning ML
     # ==================================
 
     (ml_model_name, ml_model) = model_type(ml_model, device, smi_dict, max_length_smi)
-    logging.info(f"Summary of ml model: {ml_model} ")
+    logging.info(f"Summary of ml model used for the prediction: {ml_model} ")
     file_path = f"maxsmi/prediction_models/{args.task}"
     ml_model.load_state_dict(
         torch.load(f"{file_path}/model_dict.pth", map_location=device)
