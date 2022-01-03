8aa4a23e8044c2d806a11630ed26cd607064de38
t-kimber <talia.kimber@gmail.com>
2022-01-03 09:40:49 +0100

    :white_check_mark: Update test.

---
+++ maxsmi/tests/test_utils_parser.py
@@ -0,0 +1,59 @@
+"""
+Unit and regression test for the maxsmi package.
+"""
+
+# Import package, test suite, and other packages as needed
+# import maxsmi
+import pytest
+import sys
+from maxsmi.utils.utils_parser import string_to_bool, augmentation_strategy
+from maxsmi.augmentation_strategies import (
+    no_augmentation,
+    augmentation_with_duplication,
+    augmentation_without_duplication,
+    augmentation_with_reduced_duplication,
+    augmentation_maximum_estimation,
+)
+
+
+def test_maxsmi_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "maxsmi" in sys.modules
+
+
+####################
+@pytest.mark.parametrize(
+    "string, solution",
+    [("yes", True), ("True", True), ("False", False), (True, True), (False, False)],
+)
+def test_string_to_bool(string, solution):
+    result = string_to_bool(string)
+    assert solution == result
+
+
+def test_string_to_bool_exception():
+    with pytest.raises(Exception):
+        assert string_to_bool("Fasle")
+
+
+@pytest.mark.parametrize(
+    "string, solution",
+    [
+        ("no_augmentation", no_augmentation),
+        ("augmentation_with_duplication", augmentation_with_duplication),
+        ("augmentation_without_duplication", augmentation_without_duplication),
+        (
+            "augmentation_with_reduced_duplication",
+            augmentation_with_reduced_duplication,
+        ),
+        ("augmentation_maximum_estimation", augmentation_maximum_estimation),
+    ],
+)
+def test_augmentation_strategy(string, solution):
+    result = augmentation_strategy(string)
+    assert solution == result
+
+
+def test_augmentation_strategy_exception():
+    with pytest.raises(Exception):
+        assert augmentation_strategy("augment")
